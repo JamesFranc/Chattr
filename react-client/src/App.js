03bc4040066f9fd5d922d0d0788dfbbbfc0b05ea
Jimmy <francis.jamesii@gmail.com>
2022-01-24 06:58:39 -0700

    pass data from child to parent and render

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
 import UserSelect from './features/chat/components/UserSelect';
 import ChatBox from './features/chat/components/ChatBox';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('');
+  const handleCallback = function(user) {
+    setUser(user);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <UserSelect/>
-        <ChatBox selectedUser="Nemo"/>
+        <UserSelect parentCallBack={handleCallback}/>
+        <ChatBox selectedUser={user}/>
       </header>
     </div>
   );
--- react-client/src/features/chat/components/UserSelect.jsx
+++ react-client/src/features/chat/components/UserSelect.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-function UserSelect() {
-    const [user, setUser] = useState('');
-    const changeHandler = function(e) {
-        console.log(`${e.target.value} was selected!`);
-    }
+
+function UserSelect(props) {
+
     const users = [
         {name: 'Nemo', key:1}, 
         {name: 'Pierre', key: 2}
     ].map((user) => <option key={user.key} value={user.name}>{user.name}</option>);
     
+    const changeHandler = function(e) {
+        props.parentCallBack(e.target.value);
+        e.preventDefault();
+    }
 
     return (
     <div>
