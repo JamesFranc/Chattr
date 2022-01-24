dcd33a7bb22f09c66b6fbb3ce69020813034d549
Jimmy <francis.jamesii@gmail.com>
2022-01-23 20:49:29 -0700

    add reducer, slice and UserSelect function

--- react-client/src/features/chat/components/UserSelect.jsx
+++ react-client/src/features/chat/components/UserSelect.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-class UserSelect extends React.Component {
-    render () {
-        const users = [
-            {name: 'Nemo', key:1}, 
-            {name: 'Pierre', key: 2}
-        ].map((user) => <option key={user.key} value={user.name}>{user.name}</option>);
-        return (
-            <div>
-                <select>
-                    {users}
-                </select>       
-            </div>
-        )
+import React, { useState } from 'react';
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+function UserSelect() {
+    const [user, setUser] = useState('');
+    const changeHandler = function(e) {
+        console.log(`${e.target.value} was selected!`);
     }
+    const users = [
+        {name: 'Nemo', key:1}, 
+        {name: 'Pierre', key: 2}
+    ].map((user) => <option key={user.key} value={user.name}>{user.name}</option>);
+    
+
+    return (
+    <div>
+        <select onChange={changeHandler}>
+            {users}
+        </select>       
+    </div>
+    );
 }
+
 export default UserSelect;
